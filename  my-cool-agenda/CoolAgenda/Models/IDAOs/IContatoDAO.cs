02c20a2757a9ddea6d7b96ad57e19c773ee7a9ea
leleeyoshi@gmail.com <leleeyoshi@gmail.com@7d95635a-5a6c-8383-3db3-eca68033d6a1>
2014-11-04 19:24:42 +0000

    IDAOS

---
+++  my-cool-agenda/CoolAgenda/Models/IDAOs/IContatoDAO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Data.OleDb;
+using System.Linq;
+using System.Web;
+
+namespace CoolAgenda.Models.IDAOs
+{
+    public class IContatoDAO
+    {
+
+    }
+}
---
+++  my-cool-agenda/CoolAgenda/Models/IDAOs/INotaDAO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Data.OleDb;
+using System.Linq;
+using System.Web;
+
+namespace CoolAgenda.Models
+{
+    public class INotaDAO
+    {
+    }
+}
