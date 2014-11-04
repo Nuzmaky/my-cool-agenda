56bb2d8612895258b9c160f5e1a84e206760469b
leleeyoshi@gmail.com <leleeyoshi@gmail.com@7d95635a-5a6c-8383-3db3-eca68033d6a1>
2014-11-04 19:25:05 +0000

    ISERVICES, SERVICES

---
+++  my-cool-agenda/CoolAgenda/Models/IServices/ITarefaService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace CoolAgenda.Models
+{
+    public class ITarefaService
+    {
+    }
+}
